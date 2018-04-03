a498b38e392b6a5aaa19906861d70e07935cf788
KolbeML <kleona3@students.towson.edu>
2018-04-03 16:55:13 -0400

    Fix css
    Fixed css/html as appropriate; still having issues with pdf generation (margins/padding/lack of centering)

--- test.js
+++ test.js
@@ -1,12 +1,22 @@
-var htmlToPdf = require('html-to-pdf');
-htmlToPdf.convertHTMLFile('test.html', '../test.pdf',
-    function (error, success) {
-       if (error) {
-            console.log('Oh noes! Errorz!');
-            console.log(error);
-        } else {
-            console.log('Woot! Success!');
-            console.log(success);
-        }
-    }
-);
+//var htmlToPdf = require('html-to-pdf');
+//htmlToPdf.convertHTMLFile('testy.html', '../test.pdf',
+    //function (error, success) {
+      // if (error) {
+         //   console.log('Oh noes! Errorz!');
+         //   console.log(error);
+       // } else {
+        //    console.log('Woot! Success!');
+        //    console.log(success);
+      //  }
+  //  }
+//);
+
+var fs = require('fs');
+var pdf = require('html-pdf');
+var html = fs.readFileSync('testy.html', 'utf8');
+var options = { format: 'Letter' };
+ 
+pdf.create(html, options).toFile('./test.pdf', function(err, res) {
+  if (err) return console.log(err);
+  console.log(res); // { filename: '/app/testy.pdf' }
+});
