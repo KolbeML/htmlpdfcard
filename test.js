94711e81fcec1c003e801f3179c5a782c68c5b0a
KolbeML <kleona3@students.towson.edu>
2018-04-04 10:39:26 -0400

    Add pdf-filling functionality
    Added pdffiller npm package, you can now customize the sheet of cards

--- test.js
+++ test.js
@@ -1,22 +1,43 @@
-//var htmlToPdf = require('html-to-pdf');
-//htmlToPdf.convertHTMLFile('testy.html', '../test.pdf',
-    //function (error, success) {
-      // if (error) {
-         //   console.log('Oh noes! Errorz!');
-         //   console.log(error);
-       // } else {
-        //    console.log('Woot! Success!');
-        //    console.log(success);
-      //  }
-  //  }
-//);
+var pdfFiller = require('pdffiller');
+var name = "Sidd Kaza";
+var org = "Towson University";
+var url = "clark.center/Sidd";
+var sourcePDF = "businesscardformempty.pdf";
+var destinationPDF = "test_complete.pdf";
+var data = {
+  Name2: name,
+  Org2: org,
+  Org1: org,
+  Name3: name,
+  Org3: org,
+  Name4: name,
+  Org4: org,
+  Name5: name,
+  Org5: org,
+  Name6: name,
+  Org6: org,
+  Name7: name,
+  Org7: org,
+  Name8: name,
+  Org8: org,
+  Name9: name,
+  Org9: org,
+  Name10: name,
+  Org10: org,
+  Name1: name,
+  URL1: url,
+  URL2: url,
+  URL9: url,
+  URL10: url,
+  URL3: url,
+  URL4: url,
+  URL5: url,
+  URL6: url,
+  URL7: url,
+  URL8: url
+};
 
-var fs = require('fs');
-var pdf = require('html-pdf');
-var html = fs.readFileSync('testy.html', 'utf8');
-var options = { format: 'Letter' };
- 
-pdf.create(html, options).toFile('./test.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/testy.pdf' }
+pdfFiller.fillForm(sourcePDF, destinationPDF, data, function (err) {
+  if (err) throw err;
+  console.log("In callback (we're done).");
 });
